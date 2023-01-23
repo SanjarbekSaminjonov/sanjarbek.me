7c4efe37c4c4f6fbe42d0a838c44b4db29784d7d
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2023-01-23 10:17:03 +0500

    ...

--- src/apiCalls/axios.ts
+++ src/apiCalls/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'http://api.saminjonov.uzm';
 
 export const getData = (pathname: string) => {
-  return axios.get(BASE_URL + pathname)
+  return axios.get(BASE_URL + pathname + '/')
     .then((response) => response.data)
     .catch((error) => {
       console.log(error);
