a91f85a7ef448f80282e92a329b44aa870134272
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2023-01-23 22:24:26 +0500

    Add old version link

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,14 @@ import React from "react";
 export const Home = () => {
   return (
     <div className={"min-h-screen min-w-screen flex items-center justify-center"}>
-      <Link className={"text-4xl underline hover:text-blue-600 active:text-sky-700"} to="/academy">Academy</Link>
+      <div className={"text-center"}>
+        <p className={"text-gray-500 mb-5"}>This web site is still in development mode</p>
+        <a className={"text-4xl underline hover:text-blue-600 active:text-sky-700"}
+           href={"https://github.saminjonov.uz"}>Go old version</a>
+        <br/>
+        <br/>
+        <Link className={"text-2xl text-white hover:text-blue-600 hover:underline"} to="/academy">Academy</Link>
+      </div>
     </div>
   )
 }
