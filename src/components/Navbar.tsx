3bbe47ed1531b03ac90e1c2934a9938764312cdd
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2023-01-29 13:01:32 +0500

    Backgroud color and navbar devider

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,12 +7,12 @@ export const Navbar = ({items, currentPath}: NavbarProps) => {
     <div className={"container container-sm mx-auto max-w-4xl my-12 px-4 lg:px-0"}>
       <ul className={"flex items-center flex-wrap text-xl"}>
         <li key={"Home"} className={"mr-2"}>
-          <Link className={"text-sky-400 underline"} to={"/"}>Home</Link> {" > "}
+          <Link className={"text-sky-400 underline"} to={"/"}>Home</Link> {" / "}
         </li>
         {items.map((item) => {
           return (
             <li className={"mr-2"} key={item.link}>
-              <Link className={"text-sky-400 underline"} to={item.link}>{item.title}</Link> {" > "}
+              <Link className={"text-sky-400 underline"} to={item.link}>{item.title}</Link> {" / "}
             </li>
           )
         })}
